b65ef7b4c776c72ac82f5aeffbe2ce31e9c878dc
Maximilian Unsinn <masterbaron@web.de>
2025-12-22 19:05:32 +0100

    Fix DarkmoonCanon 9

--- src/server/scripts/MoDCore/DarkmoonIsland/DarkmoonCanon.cpp
+++ src/server/scripts/MoDCore/DarkmoonIsland/DarkmoonCanon.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include "DB2Stores.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "ScriptedGossip.h"
