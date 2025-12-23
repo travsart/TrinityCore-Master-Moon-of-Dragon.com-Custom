a57e74ed6f3b27a53476a66733e1cde47d77121c
Maximilian Unsinn <masterbaron@web.de>
2025-12-23 17:35:32 +0100

    Fix zone_gilneas

--- src/server/scripts/MoDCore/EasternKingdoms/zone_gilneas.h
+++ src/server/scripts/MoDCore/EasternKingdoms/zone_gilneas.h
@@ -15,6 +15,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+#include "Common.h"
 
 #ifndef GILNEAS_M_
 #define GILNEAS_M_
