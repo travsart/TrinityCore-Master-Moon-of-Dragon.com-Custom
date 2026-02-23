e07fe8023372c009d9e0b083aad4efa94768496c
Travis <travsart27@gmail.com>
2026-02-23 15:21:19 -0500

    typo

--- src/modules/Playerbot/Lifecycle/BotSpawner.h
+++ src/modules/Playerbot/Lifecycle/BotSpawner.h
@@ -217,8 +217,7 @@ private:
 
     // Character creation
     ObjectGuid CreateCharacterForAccount(uint32 accountId, SpawnRequest const& request);
-    bool MeetsChrCustomizationReq(ChrCustomizationReqEntry const* req, Races race, Classes playerClass,bool checkRequiredDependentChoices, Trinity::IteratorPair<UF::ChrCustomizationChoice const*> selectedChoices) const
-    // NOTE: CreateBotCharacter moved to public section for InstanceBotPool access
+    bool MeetsChrCustomizationReq(ChrCustomizationReqEntry const* req, Races race, Classes playerClass,bool checkRequiredDependentChoices, Trinity::IteratorPair<UF::ChrCustomizationChoice const*> selectedChoices) const;    // NOTE: CreateBotCharacter moved to public section for InstanceBotPool access
 
     // Population calculations
     void CalculateZoneTargets();
