f0ccb18b2ec7a4eb1b1862f71250440c9c1b3916
Travis <travsart27@gmail.com>
2026-03-31 21:11:46 -0400

    update

--- sql/RoleplayCore/2.1.hotfixes_db_spell_changes.sql
+++ sql/RoleplayCore/2.1.hotfixes_db_spell_changes.sql
@@ -1,5 +1,13 @@
 USE `hotfixes`;
 
-REPLACE INTO `spell_misc` VALUES (170768, 0, 268435456, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 21, 0, 1, 1, 0, 0, 0, 988194, 0, 0, 0, 0, 0, 196742, 60257);
-
+REPLACE INTO `spell_misc` (
+    `ID`, 
+    `Attributes1`, `Attributes2`, `Attributes3`, `Attributes4`, `Attributes5`, `Attributes6`, `Attributes7`, `Attributes8`, `Attributes9`, `Attributes10`, 
+    `Attributes11`, `Attributes12`, `Attributes13`, `Attributes14`, `Attributes15`, `Attributes16`, `Attributes17`, 
+    `DifficultyID`, `CastingTimeIndex`, `DurationIndex`, `PvPDurationIndex`, `RangeIndex`, `SchoolMask`, `Speed`, `LaunchDelay`, `MinDuration`, `SpellIconFileDataID`, 
+    `ActiveIconFileDataID`, `ContentTuningID`, `ShowFutureSpellPlayerConditionID`, `SpellVisualScript`, `ActiveSpellVisualScript`, `SpellID`, `VerifiedBuild`) 
+    VALUES (170768, 
+    0, 268435456, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 1, 21, 0, 1, 1, 0, 0, 0, 988194, 0, 0, 0, 0, 0, 196742, 60257);
 REPLACE INTO `hotfix_data` VALUES (170768, 170768, 3322146344, 170768, 1, 60257);
+
